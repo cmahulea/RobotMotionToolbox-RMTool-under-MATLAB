1cc47682a513d42e7002a87af8609e9e27f61c68
Sofia <sofia.hustiu@yahoo.com>
2022-02-15 12:30:32 +0100

    Update rmt_construct_MILPdiftraj.m

--- environment/rmt_construct_MILPdiftraj.m
+++ environment/rmt_construct_MILPdiftraj.m
@@ -7,25 +7,46 @@ No_r = size(Run_cells,1);
 mf = zeros(nplaces,1);
 mf(Run_cells(:,end)) = 1; % final marking in PN
 
+
+% Pre = zeros(5,8);
+% Post = zeros(5,8);
+% C=[-1 1 0 0 0 0 0 0;...
+%     1 -1 -1 1 0 0 0 0;...
+%     0 0 1 -1 -1 1 0 0;...
+%     0 0 0 0 0 0 1 -1;...
+%     0 0 0 0 1 -1 -1 1];
+% Post = [0 1 0 0 0 0 0 0;...
+%     1 0 0 1 0 0 0 0;...
+%     0 0 1 0 0 1 0 0;...
+%     0 0 0 0 0 0 1 0;...
+%     0 0 0 0 1 0 0 1];
+
+C = Post - Pre;
+% m0 = [1 1 0  0 0]';
+% mf = [0 0 0  1 1]';
 %the variables are: [m_0^1 m_1 sigma_1 m_0^2 m_2 sigma_2 ... m_0^|R| m_|R| sigma_|R|]
 Aeq = [];
 beq = [];
 A = [];
 b = [];
-
+% nplaces = 5;
+% ntrans = 8;
+% No_r = 2;
 temp_zeros = zeros(nplaces,2*nplaces + ntrans);
 temp_mi = [zeros(nplaces) eye(nplaces) zeros(nplaces,ntrans)]; % is considered only the variable m_i
 temp_m0 = [eye(nplaces) zeros(nplaces) zeros(nplaces, ntrans)]; % is considered only the variable m0^i
-
+temp_m0_single = [ones(1,nplaces) zeros(1,nplaces), zeros(1,ntrans)];
+temp_mi_single = [ zeros(1,nplaces) ones(1,nplaces) zeros(1,ntrans)];
+tic
 for i = 1:No_r
 
     % a) state equation
-    Aeq = [Aeq; repmat(temp_zeros,[1, i-1]) eye(nplaces) -eye(nplaces) (Post - Pre) repmat(temp_zeros,[1, No_r - i])];
+    Aeq = [Aeq; repmat(temp_zeros,[1, i-1]) eye(nplaces) -eye(nplaces) C repmat(temp_zeros,[1, No_r - i])];
     beq = [beq; zeros(nplaces,1)];
 
     % b)
     if i < No_r
-        A = [A; repmat(temp_mi,[1,i]) zeros(nplaces,2*nplaces) Post repmat(temp_zeros,[1, No_r - i - 1])];
+        A = [A; repmat(temp_mi,[1,i-1]) zeros(nplaces,2*nplaces) Post repmat(temp_m0,[1, No_r - i])];
         b = [b; ones(nplaces,1)];
     else 
        A = [A; repmat(temp_mi,[1,i-1]) zeros(nplaces,2*nplaces) Post];
@@ -33,12 +54,12 @@ for i = 1:No_r
     end
 
     % e)
-    Aeq = [Aeq; repmat(temp_zeros,[1, i-1]) temp_m0 repmat(temp_zeros,[1, No_r - i])];
-    beq = [beq; ones(nplaces,1)];
+    Aeq = [Aeq; repmat(zeros(1,2*nplaces+ntrans),[1, i-1]) temp_m0_single repmat(zeros(1,2*nplaces+ntrans),[1, No_r - i])];
+    beq = [beq; ones(1,1)];
 
     % f)
-    Aeq = [Aeq; repmat(temp_zeros,[1, i-1]) temp_mi repmat(temp_zeros,[1, No_r - i])];
-    beq = [beq; ones(nplaces,1)];
+    Aeq = [Aeq; repmat(zeros(1,2*nplaces+ntrans),[1, i-1]) temp_mi_single repmat(zeros(1,2*nplaces+ntrans),[1, No_r - i])];
+    beq = [beq; ones(1,1)];
 
 end
 
@@ -54,7 +75,7 @@ cost = [];
 for i = 1:No_r
     cost = [cost zeros(1,2*nplaces) ones(1,ntrans)];
 end
-
+toc
 
 vartype = '';
 for r = 1:No_r
@@ -66,5 +87,7 @@ for r = 1:No_r
         vartype = sprintf('%sI',vartype); %put the sigma as integer
     end
 end
-[xmin,~,exitflag] = cplexmilp(cost,A,b,Aeq,beq,[],[],[],zeros(1,size(A,2)),[],vartype);
 
+tic
+[xmin,~,exitflag] = cplexmilp(cost,A,b,Aeq,beq,[],[],[],zeros(1,size(A,2)),[],vartype);
+toc
