221ab332d38c2ce04d51c63db18a5fd53d9e0d6c
UZ-GISED <cmahulea@unizar.es>
2024-09-11 20:12:30 +0200

    Update rectangular decomposition
    Avoid to use cddmex and use the matlab builtin objects polyshape

--- environment/rmt_rectangular_decomposition.m
+++ environment/rmt_rectangular_decomposition.m
@@ -1,4 +1,4 @@
-%    This is part of RMTool - Robot Motion Toolbox, for Matlab 2010b or newer.
+%    This is part of RMTool - Robot Motion Toolbox, for Matlab 2018b or newer.
 %
 %    Copyright (C) 2016 RMTool developing team. For people, details and citing 
 %    information, please see: http://webdiis.unizar.es/RMTool/index.html.
@@ -20,16 +20,17 @@
 %   MOBILE ROBOT TOOLBOX
 %   Graphical User Interface
 %   First version released on September, 2014. 
-%   Last modification December 29, 2015.
+%   Second version released on September 2024
+%   Last modification September 11, 2024.
 %   More information: http://webdiis.unizar.es/RMTool
 % ============================================================================
 
 
 function [C,adj,varargout]=rmt_rectangular_decomposition(objects,env_bounds,varargin)
-%mk, version: may 2012 (obstacles may intersect or go outside environment)
-%rectangular decomposition, using quad-tree idea
-%env_bounds has the form [x_min,x_max,y_min,y_max]
-%prec is the max number of rectangle on each axis (power of 2); if not specified, it is 32 by default
+% version: septemer 2024 (obstacles may intersect or go outside environment)
+% rectangular decomposition, using quad-tree idea
+% env_bounds has the form [x_min,x_max,y_min,y_max]
+% prec is the max number of rectangle on each axis (power of 2); if not specified, it is 32 by default
 
 if nargin==2    %precision was not specified
     prec=32;
@@ -39,27 +40,17 @@ end
 
 warning('off', 'MATLAB:mex:deprecatedExtension');
 
-%convert obstacles (all are convex hulls) in H-representation
-n_ob=length(objects);
-A=cell(n_ob,1);
-b=cell(n_ob,1);
+% convert obstacles (all are convex hulls) in H-representation
+polys=cell(length(objects));    %matlab polyshapes of the obstacles
 for i=1:length(objects)
-    M=objects{i}';
-    H=cddmex('hull',struct('V',M)); %H-representation
-    A{i}=H.A;
-    b{i}=-H.B;  %object i: Ax+b<=0;
+    polys{i} = polyshape(objects{i}');
 end
 
 prec_x=(env_bounds(2)-env_bounds(1))/prec;  %minimum size on x-axis
 prec_y=(env_bounds(4)-env_bounds(3))/prec;  %minimum size on y-axis
 
-% %limits for world:
-% x1=x_min;
-% x2=x_max;
-% y1=y_min;
-% y2=y_max;
 C={};    %start with empty cell for decomposition
-C = split_check_rectangle(C, A, b, prec_x, prec_y, env_bounds(1), env_bounds(2), env_bounds(3), env_bounds(4)); %start with whole world, which will be recursively split in subrectangles
+C = split_check_rectangle(C, polys, prec_x, prec_y, env_bounds(1), env_bounds(2), env_bounds(3), env_bounds(4)); %start with whole world, which will be recursively split in subrectangles
 
 %find adjacency, based on coord
 k=length(C);    %number of rectangles
@@ -73,25 +64,26 @@ if nargout>2    %if desired, compute middle points between adjacent cells (usefu
     end
 end
 
-Ak=[0 -1;-1 0;0 1;1 0]; %matrix A for H-repres of each rectangle
+C_poly = cell(k);
+for i = 1 : k
+    C_poly{i} = polyshape(C{i}');
+end
 for i=1:(k-1)
-    bi=[C{i}(2,1) C{i}(1,1) -C{i}(2,3) -C{i}(1,2)]';    %H-repres of rectangle i (y1,x1,-y2,-x2) - based on teh way vertices were added in cell C
     for j=(i+1):k
-        bj=[C{j}(2,1) C{j}(1,1) -C{j}(2,3) -C{j}(1,2)]';    %H-repres of rectangle j
-        V=cddmex('extreme',struct('A',[Ak;Ak],'B',[-bi;-bj]));
-        if size(V.V,1)==2   %rectangles i and j are adjacent iff their intersection is a line segment (2 vertces)
+        %figure;plot(polyshape(C{i}'));hold on; plot(polytemp);
+        [~,TFon]=isinterior(C_poly{i},C_poly{j}.Vertices);
+        if (sum(TFon)==2)   %rectangles i and j are adjacent iff their intersection is a line segment (2 vertces)
             adj(i,j)=1;
             adj(j,i)=1;
-
             if nargout>2
                 %middle of segment between cells i and j:
-                middle_temp=mean(V.V);
+                middle_temp=mean(C_poly{j}.Vertices(find(TFon),:));
                 middle_X(i,j)=middle_temp(1);
                 middle_X(j,i)=middle_temp(1);
                 middle_Y(i,j)=middle_temp(2);
                 middle_Y(j,i)=middle_temp(2);
                 if nargout>4    %common line segments shared by adjacent cells
-                    com_F{i,j}=sortrows(V.V)';  %com_F{i,j} has form [x_i' , x_i'' ; y_i' , y_i''], x_i'<=x_i''
+                    com_F{i,j}=C_poly{j}.Vertices(find(TFon))';  %com_F{i,j} has form [x_i' , x_i'' ; y_i' , y_i''], x_i'<=x_i''
                     com_F{j,i}=com_F{i,j};
                 end
             end
@@ -101,10 +93,10 @@ end
 % adj=sparse(adj);
 
 %arrange vertices of each cell from decomposition to be convex hull
-for i=1:length(C)
-    ch_in=convhull(C{i}(1,:),C{i}(2,:));
-    C{i}=C{i}(:,ch_in(1:length(ch_in)-1));
-end
+%for i=1:length(C)
+%    ch_in=convhull(C{i}(1,:),C{i}(2,:));
+%    C{i}=C{i}(:,ch_in(1:length(ch_in)-1));
+%end
 
 if nargout>2
     %return middle of adjacent segments:
@@ -119,40 +111,27 @@ end
 warning('on', 'MATLAB:mex:deprecatedExtension');
 
 
-function C = split_check_rectangle(C, A, b, prec_x, prec_y, x1,x2,y1,y2)
-%check if current rectalgle is free, occupied or mixed; if free->add it to cell decomposition, if mixed reccursively split
+function C = split_check_rectangle(C, polys, prec_x, prec_y, x1,x2,y1,y2)
+%check if current rectangle is free, occupied or mixed; if free->add it to cell decomposition, if mixed reccursively split
 %argumets: C is currnet cell decomposition, x1,x2,y1,y2 limits of current rectangle, prec_x,prec_y minimum size of x or y
 
-%Current rectangle: (first is world):
-% M=[x1 y1; x2 y1; x2 y2; x1 y2]; %world vertices
-% H=cddmex('hull',struct('V',M)); %H-representation
-% R_A=H.A;  %world (current rectangle) in H-representation info
-% R_b=-H.B;
-R_A=[0 -1;-1 0;0 1;1 0];    %avoid the commented computations
-R_b=[y1 x1 -y2 -x2]';
-
-if (x2-x1)<prec_x || (y2-y1)<prec_y
+if (((x2-x1)<prec_x) || ((y2-y1)<prec_y))
     return; %precision reached for current sub-splitting
 end
+rectangle_to_check = polyshape([x1 x2 x2 x1],[y1 y1 y2 y2]); %rectangle to check
 
-free=1; %assume rectangle R is free
 area_int=0; %area of intersections of current rectangle with obstacles
-for i=1:length(A);
-    V=cddmex('extreme',struct('A',[A{i};R_A],'B',[-b{i};-R_b]));
-    if (size(V.V,1) > 2) && (rank([V.V , ones( size(V.V,1), 1)]) == 3) %intersection is a full polytope
-        free=0; %rectangle is certainly not free; it may be mixed or occupied
-        [ignore, area]=convhull(V.V(:,1),V.V(:,2)); %area of current intersection
-        area_int = area_int+area;
-    end
+for i=1:length(polys)
+    area_int = area_int + area(intersect(polys{i},rectangle_to_check));
 end
-if free==1  %rectangle is free, add its vertices to cell decomposition
+
+if ( area_int < (prec_x*prec_y/100) ) %rectangle is free, add its vertices to cell decomposition
     C{end+1}=[x1 x2 x2 x1;y1 y1 y2 y2];
 elseif abs(area_int-(x2-x1)*(y2-y1)) >= (prec_x*prec_y/100)  %rectangle is mixed or occupied; decide which one after area of intersection
     %mixed rectangle, must be split in 4:
-    C = split_check_rectangle(C,A,b,prec_x,prec_y, x1, (x1+x2)/2, y1, (y1+y2)/2); %lower-left rectangle
-    C = split_check_rectangle(C,A,b,prec_x,prec_y, (x1+x2)/2, x2, y1, (y1+y2)/2); %lower-right rectangle
-    C = split_check_rectangle(C,A,b,prec_x,prec_y, (x1+x2)/2, x2, (y1+y2)/2, y2); %upper-right rectangle
-    C = split_check_rectangle(C,A,b,prec_x,prec_y, x1, (x1+x2)/2, (y1+y2)/2, y2); %upper-left rectangle
-    
+    C = split_check_rectangle(C,polys,prec_x,prec_y, x1, (x1+x2)/2, y1, (y1+y2)/2); %lower-left rectangle
+    C = split_check_rectangle(C,polys,prec_x,prec_y, (x1+x2)/2, x2, y1, (y1+y2)/2); %lower-right rectangle
+    C = split_check_rectangle(C,polys,prec_x,prec_y, (x1+x2)/2, x2, (y1+y2)/2, y2); %upper-right rectangle
+    C = split_check_rectangle(C,polys,prec_x,prec_y, x1, (x1+x2)/2, (y1+y2)/2, y2); %upper-left rectangle  
     %if rectangle is occupied (areas almost equal), do nothing - no need for else
 end
